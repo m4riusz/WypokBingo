c545d5f794ca88c3d00ad8b6e6b0b7d9cf6bcad8
msut <sariuszm@gmail.com>
2017-03-06 22:19:04 +0100

    add create user method

--- src/main/java/pl/wykop/service/UserService.java
+++ src/main/java/pl/wykop/service/UserService.java
@@ -2,6 +2,10 @@ package pl.wykop.service;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
+import pl.wykop.dto.UserCreateForm;
+import pl.wykop.dto.UserDto;
+import pl.wykop.exception.UserCreateException;
+import pl.wykop.exception.UserException;
 
 /**
  * Created by mariusz on 06.03.17.
@@ -9,4 +13,5 @@ import org.springframework.stereotype.Service;
 @Service
 public interface UserService extends UserDetailsService{
 
+    UserDto create(UserCreateForm userCreateForm) throws UserCreateException;
 }
--- src/main/java/pl/wykop/service/UserServiceImpl.java
+++ src/main/java/pl/wykop/service/UserServiceImpl.java
@@ -2,21 +2,44 @@ package pl.wykop.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import pl.wykop.domain.User;
+import pl.wykop.dto.UserCreateForm;
+import pl.wykop.dto.UserDto;
+import pl.wykop.exception.UserCreateException;
+import pl.wykop.mapper.UserMapper;
 import pl.wykop.repository.UserRepository;
+
+import javax.transaction.Transactional;
 
 /**
  * Created by mariusz on 06.03.17.
  */
 @Service
+@Transactional
 @AllArgsConstructor
 public class UserServiceImpl implements UserService {
 
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public User loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException(username));
+    }
+
+    @Override
+    public UserDto create(UserCreateForm userCreateForm) throws UserCreateException {
+        if (userRepository.findByUsername(userCreateForm.getUsername()).isPresent()) {
+            throw new UserCreateException("User with this username already exists!");
+        } else if (userRepository.findByEmail(userCreateForm.getEmail()).isPresent()) {
+            throw new UserCreateException("User with this email already exists!");
+        }
+        User user = new User();
+        user.setUsername(userCreateForm.getUsername());
+        user.setEmail(userCreateForm.getEmail());
+        user.setPassword(passwordEncoder.encode(userCreateForm.getPassword()));
+        return UserMapper.INSTANCE.userToUserDto(userRepository.save(user));
     }
 }
