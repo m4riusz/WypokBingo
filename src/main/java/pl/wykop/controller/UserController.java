4e765173e7dd2a247ef9e6744462f52b4f00dabd
msut <sariuszm@gmail.com>
2017-03-06 22:19:27 +0100

    add simple user controller

---
+++ src/main/java/pl/wykop/controller/UserController.java
@@ -0,0 +1,27 @@
+package pl.wykop.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import pl.wykop.dto.UserCreateForm;
+import pl.wykop.dto.UserDto;
+import pl.wykop.exception.UserCreateException;
+import pl.wykop.service.UserService;
+
+/**
+ * Created by mariusz on 06.03.17.
+ */
+
+@RestController
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @RequestMapping(value = "/user", method = RequestMethod.POST)
+    public UserDto createUser(@RequestBody UserCreateForm userCreateForm) throws UserCreateException {
+        return userService.create(userCreateForm);
+    }
+}
