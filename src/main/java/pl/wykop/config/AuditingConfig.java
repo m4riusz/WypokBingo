38d120e8a70bb505751f805b1f08be62e40ae77e
msut <sariuszm@gmail.com>
2017-03-06 22:20:52 +0100

    add auditing configuration for @CreatedDate etc

---
+++ src/main/java/pl/wykop/config/AuditingConfig.java
@@ -0,0 +1,13 @@
+package pl.wykop.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+/**
+ * Created by mariusz on 06.03.17.
+ */
+@Configuration
+@EnableJpaAuditing
+public class AuditingConfig {
+
+}
