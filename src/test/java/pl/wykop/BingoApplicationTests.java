d592b12ef4e68dcc20e33bf920df3a5ef3f7915d
msut <sariuszm@gmail.com>
2017-03-06 16:07:38 +0100

    init commit

---
+++ src/test/java/pl/wykop/BingoApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.wykop;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BingoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
