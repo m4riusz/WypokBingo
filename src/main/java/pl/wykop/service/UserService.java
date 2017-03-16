679e2b1ae6c27cce87125729135d1e556a3d179f
msut <sariuszm@gmail.com>
2017-03-16 22:14:15 +0100

    add username and username like methods

--- src/main/java/pl/wykop/service/UserService.java
+++ src/main/java/pl/wykop/service/UserService.java
@@ -1,17 +1,33 @@
 package pl.wykop.service;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import pl.wykop.dto.UserCreateForm;
 import pl.wykop.dto.UserDto;
 import pl.wykop.exception.UserCreateException;
-import pl.wykop.exception.UserException;
+import pl.wykop.exception.UserNotFoundException;
+
+import javax.transaction.Transactional;
+import java.awt.print.Pageable;
+import java.util.List;
 
 /**
  * Created by mariusz on 06.03.17.
  */
 @Service
-public interface UserService extends UserDetailsService{
+@Transactional
+public interface UserService extends UserDetailsService {
 
+    @PreAuthorize("isAnonymous()")
     UserDto create(UserCreateForm userCreateForm) throws UserCreateException;
+
+    @PreAuthorize("isAuthenticated()")
+    UserDto findByUsername(String username) throws UserNotFoundException;
+
+    @PreAuthorize("isAuthenticated()")
+    List<UserDto> findByNameLike(String username, Pageable pageable);
+
+    @PreAuthorize("isAuthenticated()")
+    UserDto getCurrentUser();
 }
--- src/main/java/pl/wykop/service/UserServiceImpl.java
+++ src/main/java/pl/wykop/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package pl.wykop.service;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -9,10 +9,15 @@ import pl.wykop.domain.User;
 import pl.wykop.dto.UserCreateForm;
 import pl.wykop.dto.UserDto;
 import pl.wykop.exception.UserCreateException;
+import pl.wykop.exception.UserNotFoundException;
 import pl.wykop.mapper.UserMapper;
 import pl.wykop.repository.UserRepository;
 
 import javax.transaction.Transactional;
+import java.awt.print.Pageable;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 /**
  * Created by mariusz on 06.03.17.
@@ -42,5 +47,21 @@ public class UserServiceImpl extends AbstractService implements UserService {
         user.setEmail(userCreateForm.getEmail());
         user.setPassword(passwordEncoder.encode(userCreateForm.getPassword()));
         return UserMapper.INSTANCE.userToUserDto(userRepository.save(user));
+    }
+
+    @Override
+    public UserDto findByUsername(String username) throws UserNotFoundException {
+        return UserMapper.INSTANCE.userToUserDto(userRepository.findByUsername(username).orElseThrow(() -> new UserNotFoundException(getMessage("user.find.error.username"))));
+    }
+
+    @Override
+    public List<UserDto> findByNameLike(String username, Pageable pageable) {
+        return userRepository.findByUsernameLike(username, pageable).map(UserMapper.INSTANCE::userToUserDto).collect(toList());
+    }
+
+    @Override
+    public UserDto getCurrentUser() {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+        return UserMapper.INSTANCE.userToUserDto(loadUserByUsername(username));
     }
 }
