91c05f5c878cb7c2eaea11a09b9df7173eddf4b9
msut <sariuszm@gmail.com>
2017-03-16 21:36:56 +0100

    add find by name methods

--- src/main/java/pl/wykop/repository/UserRepository.java
+++ src/main/java/pl/wykop/repository/UserRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.wykop.domain.User;
 
+import java.awt.print.Pageable;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 /**
  * Created by mariusz on 06.03.17.
@@ -15,4 +17,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
     Optional<User> findByEmail(String email);
+
+    Stream<User> findByUsernameLike(String username, Pageable pageable);
 }
