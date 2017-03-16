5e495269f18fd0c2da12694918dfc704c48b1bc5
msut <sariuszm@gmail.com>
2017-03-16 22:14:51 +0100

    add req mapping for username methods

--- src/main/java/pl/wykop/config/Route.java
+++ src/main/java/pl/wykop/config/Route.java
@@ -5,7 +5,11 @@ package pl.wykop.config;
  */
 public final class Route {
 
-    public static final String AUTHENTICATED_PATTERN = "/**";
-    public static final String AUTH_URL = "/api/auth";
-    public static final String USER = "/api/user";
+    public static final String API_PREFIX = "/api";
+    public static final String AUTHENTICATED_PATTERN = API_PREFIX;
+
+    public static final String AUTH_URL = API_PREFIX + "/auth";
+    public static final String USER = API_PREFIX + "/user";
+    public static final String USER_BY_USERNAME = USER + "/{username}";
+
 }
--- src/main/java/pl/wykop/controller/UserController.java
+++ src/main/java/pl/wykop/controller/UserController.java
@@ -1,17 +1,22 @@
 package pl.wykop.controller;
 
 import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import pl.wykop.config.Route;
 import pl.wykop.dto.UserCreateForm;
 import pl.wykop.dto.UserDto;
 import pl.wykop.exception.UserCreateException;
+import pl.wykop.exception.UserNotFoundException;
 import pl.wykop.service.UserService;
 
-import java.util.Locale;
+import java.util.Optional;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+import static pl.wykop.config.Route.USER;
+import static pl.wykop.config.Route.USER_BY_USERNAME;
 
 /**
  * Created by mariusz on 06.03.17.
@@ -23,8 +28,15 @@ public class UserController {
 
     private final UserService userService;
 
-    @RequestMapping(value = Route.USER, method = RequestMethod.POST)
+    @RequestMapping(value = USER, method = POST)
     public UserDto createUser(@RequestBody UserCreateForm userCreateForm) throws UserCreateException {
         return userService.create(userCreateForm);
     }
+
+    @RequestMapping(value = {USER, USER_BY_USERNAME}, method = GET)
+    public UserDto getCurrentLoggedUser(@PathVariable Optional<String> username) throws UserNotFoundException {
+        return username.isPresent() ? userService.findByUsername(username.get()) : userService.getCurrentUser();
+    }
+
+
 }
