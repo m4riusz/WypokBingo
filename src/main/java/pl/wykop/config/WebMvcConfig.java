3cfd663c13a3db2d896ca584121e336c20ace9c3
msut <sariuszm@gmail.com>
2017-03-20 22:15:07 +0100

    override validator for hibernate message interpolation

--- src/main/java/pl/wykop/config/WebMvcConfig.java
+++ src/main/java/pl/wykop/config/WebMvcConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.validation.Validator;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -46,5 +48,17 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
         messageSource.setBasename("i18n" + File.separator + "message");
         messageSource.setDefaultEncoding("UTF-8");
         return messageSource;
+    }
+
+    @Bean
+    public LocalValidatorFactoryBean validator() {
+        LocalValidatorFactoryBean localValidatorFactoryBean = new LocalValidatorFactoryBean();
+        localValidatorFactoryBean.setValidationMessageSource(messageSource());
+        return localValidatorFactoryBean;
+    }
+
+    @Override
+    public Validator getValidator() {
+        return validator();
     }
 }
